9531617025f766e07b507f57c805502be7eebd00
Vadim <vad.general@mail.ru>
2023-03-11 09:59:50 +0300

    Feat: добавить консольный ввод данных

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,6 +1,26 @@
 fun main() {
-    var triangle = Triangle(1.0, 2.0, 1.0);
-    var circle = Circle(5.0, .0, .0);
+    var sizeA: Double;
+    var sizeB: Double;
+    var sizeC: Double;
+    println("Введите стороны треугольника");
+    sizeA = readLine()!!.toDouble();
+    sizeB = readLine()!!.toDouble();
+    sizeC = readLine()!!.toDouble();
+    var triangle = Triangle(sizeA, sizeB, sizeC);
     println(triangle.getTriangleType());
-    println(circle.isPointInsideCircle(4.0, 1.0));
+
+    var radius: Double;
+    var centerX: Double;
+    var centerY: Double;
+    var x0: Double;
+    var y0: Double;
+    println("Введите радиус и координату центра окружности X и Y");
+    radius = readLine()!!.toDouble();
+    centerX = readLine()!!.toDouble();
+    centerY = readLine()!!.toDouble();
+    var circle = Circle(radius, centerX, centerY);
+    println("Введите координаты точки, которую вы хотите проверить на принадлежность к окружности");
+    x0 = readLine()!!.toDouble();
+    y0 = readLine()!!.toDouble();
+    println(circle.isPointInsideCircle(x0, y0));
 }
