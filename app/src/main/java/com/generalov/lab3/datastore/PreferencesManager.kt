30fdf6a4515cbbb1609fcc82e082193927476411
Vadim <vad.general@mail.ru>
2023-04-28 19:07:57 +0300

    Feat: добавить хранилище данных

--- app/src/main/java/com/generalov/lab3/datastore/PreferencesManager.kt
+++ app/src/main/java/com/generalov/lab3/datastore/PreferencesManager.kt
@@ -1,4 +1,25 @@
 package com.generalov.lab3.datastore
 
-class PreferencesManager {
+import android.content.Context
+
+class PreferencesManager(context: Context) {
+    private val sharedPref = context.getSharedPreferences("my_preferences", Context.MODE_PRIVATE)
+
+    fun saveUserId(userId: Int) {
+        with(sharedPref.edit()) {
+            putInt("user_id", userId)
+            apply()
+        }
+    }
+
+    fun getUserId(): Int {
+        return sharedPref.getInt("user_id", -1)
+    }
+
+    fun removeUserId() {
+        with(sharedPref.edit()) {
+            remove("user_id")
+            apply()
+        }
+    }
 }
