baacf4f6dbbfc5923fabe261af43fb2a136eac9b
Vadim <vad.general@mail.ru>
2023-04-29 11:10:18 +0300

    efactor

--- app/src/main/java/com/generalov/lab4/screen/account/registration/RegistrationPage.kt
+++ app/src/main/java/com/generalov/lab4/screen/account/registration/RegistrationPage.kt
@@ -1,10 +1,13 @@
 package com.generalov.lab4.screen.account.registration
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -13,47 +16,36 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun RegistrationPage() {
+    val viewModel: RegistrationViewModel = viewModel()
+    val registrationState by viewModel.registrationState.collectAsState()
+
+    val resources = LocalContext.current.resources
+    val usernameError = when (registrationState) {
+        RegistrationState.UsernameEmpty -> "Поле не может быть пустым"
+        RegistrationState.UsernameShort -> "Имя пользователя слишком короткое(не менее 3 символов)"
+        RegistrationState.UsernameLong -> "Имя пользователя не может быть таким длинным(не более 20 символов)"
+        else -> ""
+    }
+
+    val passwordError = when (registrationState) {
+        RegistrationState.PasswordEmpty -> "Поле не может быть пустым"
+        RegistrationState.PasswordsDoNotMatch -> "Пароли не совпадают"
+        RegistrationState.PasswordShort -> "Пароль слишком короткий(не менее 5 символов)"
+        RegistrationState.PasswordLong -> "Пароль слишком длинный(не более 16 символов)"
+        else -> ""
+    }
+
+    val phoneError = when (registrationState) {
+        RegistrationState.PhoneNumberShort -> "Телефон должен состоять из 11 цифр"
+        RegistrationState.PhoneNumberIncorrect -> "Поле должно содержать только цифры"
+        else -> ""
+    }
+
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var repeatPassword by remember { mutableStateOf("") }
     var phone by remember { mutableStateOf("+7") }
-    var passwordError by remember { mutableStateOf("") }
-    var phoneError by remember { mutableStateOf("") }
-    var usernameError by remember { mutableStateOf("") }
     var resultInfo by remember { mutableStateOf("") }
-    val viewModel: RegistrationViewModel = viewModel()
-    val registrationState by viewModel.registrationState.collectAsState()
-    val coroutineScope = rememberCoroutineScope()
-
-    when (registrationState) {
-        RegistrationState.UsernameEmpty -> usernameError = "Поле не может быть пустым"
-        RegistrationState.UsernameShort -> usernameError =
-            "Имя пользователя слишком короткое(не менее 3 символов)"
-        RegistrationState.UsernameLong -> usernameError =
-            "Имя пользователя не может быть таким длинным(не более 20 символов)"
-        RegistrationState.PasswordEmpty -> passwordError = "Поле не может быть пустым"
-        RegistrationState.PasswordsDoNotMatch -> passwordError = "Пароли не совпадают"
-        RegistrationState.PasswordShort -> passwordError =
-            "Пароль слишком короткий(не менее 5 символов)"
-        RegistrationState.PasswordLong -> passwordError =
-            "Пароль слишком длинный(не более 16 символов)"
-        RegistrationState.PhoneNumberShort -> phoneError = "Телефон слишком короткий"
-        RegistrationState.PhoneNumberIncorrect -> phoneError = "Разрешены только цифры"
-        RegistrationState.RegistrationSuccess -> {
-            resultInfo = "Регистрация прошла успешно"
-            username = ""
-            password = ""
-            repeatPassword = ""
-            phone = ""
-            LaunchedEffect(Unit) {
-                delay(3000)
-                coroutineScope.launch {
-                    resultInfo = ""
-                }
-            }
-        }
-        else -> {}
-    }
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -71,16 +63,25 @@ fun RegistrationPage() {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            TextField(
+            OutlinedTextField(
                 value = username,
                 onValueChange = { username = it },
-                label = { Text("Имя пользователя") },
+                label = { Text("") },
+                isError = usernameError.isNotEmpty(),
+                singleLine = true,
                 modifier = Modifier.fillMaxWidth()
             )
 
+            if (usernameError.isNotEmpty()) {
+                Text(
+                    text = usernameError,
+                    color = MaterialTheme.colors.error
+                )
+            }
+
             Spacer(modifier = Modifier.height(10.dp))
 
-            TextField(
+            OutlinedTextField(
                 value = phone,
                 onValueChange = { value ->
                     if (value.length in 2..12 && value.startsWith("+7")) {
@@ -88,6 +89,11 @@ fun RegistrationPage() {
                     }
                 },
                 label = { Text("Номер телефона") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Phone
+                ),
+                isError = phoneError.isNotEmpty(),
                 modifier = Modifier.fillMaxWidth()
             )
             Text(
@@ -97,12 +103,17 @@ fun RegistrationPage() {
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            TextField(
+            OutlinedTextField(
                 value = password,
                 onValueChange = { password = it },
                 label = { Text("Пароль") },
-                modifier = Modifier.fillMaxWidth(),
-                visualTransformation = PasswordVisualTransformation()
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password
+                ),
+                isError = passwordError.isNotEmpty(),
+                modifier = Modifier.fillMaxWidth()
             )
             Text(
                 text = passwordError,
@@ -111,24 +122,37 @@ fun RegistrationPage() {
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            TextField(
+            OutlinedTextField(
                 value = repeatPassword,
                 onValueChange = { repeatPassword = it },
                 label = { Text("Повторите пароль") },
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password
+                ),
                 modifier = Modifier.fillMaxWidth(),
-                visualTransformation = PasswordVisualTransformation()
+                isError = passwordError.isNotEmpty() && repeatPassword.isNotEmpty() && repeatPassword != password
             )
+
             Spacer(modifier = Modifier.height(20.dp))
 
             Button(
                 modifier = Modifier.fillMaxWidth(),
                 onClick = {
+                    passwordError = ""
+                    usernameError = ""
+                    phoneError = ""
                     viewModel.register(
                         username = username,
                         phoneNumber = phone,
                         password = password,
                         confirmPassword = repeatPassword
                     )
+                    username = ""
+                    password = ""
+                    repeatPassword = ""
+                    phone = ""
                 }
 
             ) {
